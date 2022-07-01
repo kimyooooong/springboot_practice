880e8c3ba384b753a5027e9fc78d4fa53446bbb0
yhk <yhk@seoulauction.com>
2022-07-01 20:24:14 +0900

    어플리케이션 분리

--- src/main/java/com/kimyoong/practice/PracticeApplication.java
+++ src/main/java/com/kimyoong/practice/PracticeApplication.java
@@ -1,13 +1,25 @@
 package com.kimyoong.practice;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+@Log4j2
 @SpringBootApplication
 public class PracticeApplication {
 
+    @Value("${server.config}")
+    String config;
+
     public static void main(String[] args) {
         SpringApplication.run(PracticeApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> log.info("server config : {}", config);
     }
 
 }
